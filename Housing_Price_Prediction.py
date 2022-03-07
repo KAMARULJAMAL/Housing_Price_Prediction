537eb09e0110a09cb62013c7bd0dc19a22be9b2b
KAMARULJAMAL <94347116+KAMARULJAMAL@users.noreply.github.com>
2022-03-07 14:47:48 +0530

    First

--- Housing_Price_Prediction.py
+++ Housing_Price_Prediction.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/hello')
+def hello():
+    return 'Hello World!'
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
